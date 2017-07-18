72a6f668342b8fd156fba4a181662df13e0ceba2
le1tuan <lekoi9x@gmail.com>
2017-07-18 08:03:14 +0700

    Add babel-polyfill loader and something

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-    entry: "./src/index.js",
+    entry: 
+    [
+        "./src/index.js",
+        "babel-polyfill",
+    ],
     output:{
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
@@ -16,14 +20,42 @@ module.exports = {
                     loader: 'babel-loader',
                     options: {
                         presets: ["env","react"],
-                        plugins: [require('babel-plugin-transform-object-rest-spread')]
+                        plugins: [require('babel-plugin-transform-object-rest-spread'),require('babel-plugin-transform-class-properties')]
                     }
                 }
             },
             {
                 test: /\.css$/,
                 use: [ 'style-loader', 'css-loader' ]
-            }
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2)$/,
+                loader: 'file-loader',
+            },
+            {
+                test: /\.(jpg|png|gif)$/,
+                loaders: [
+                    'file-loader',
+                    {
+                    loader: 'image-webpack-loader',
+                    query: {
+                        mozjpeg: {
+                            progressive: true,
+                        },
+                        gifsicle: {
+                            interlaced: false,  
+                        },
+                        optipng: {
+                            optimizationLevel: 7,
+                        },
+                        pngquant: {
+                            quality: '65-90',
+                            speed: 4,
+                        },
+                    },
+                    },
+                ],
+            }   
         ]
     },
     plugins: [new HtmlWebpackPlugin({
